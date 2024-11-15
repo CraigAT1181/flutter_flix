ca224902d8af7dca189e40f511bcd614ed005816
craig <craigtipple81@gmail.com>
2024-11-15 20:46:32 +0000

    Created a Scaffold widget with a simple AppBar. Stored it in a StatelessWidget called Home. Called Home() in main().

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,25 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
+void main() => runApp(MaterialApp(home: Home()));
 
-));
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Flutter Flix!',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'Raleway-Regular',
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.red[400],
+      ),
+    );
+  }
+}
