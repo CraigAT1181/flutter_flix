f2f35b4c300b568a6b1c8a6507de1e787bb99d1e
craig <craigtipple81@gmail.com>
2024-11-16 19:58:36 +0000

    Changed theme colour. On Home, added first row. Added Text() and a placeholder for a search bar.

--- lib/main.dart
+++ lib/main.dart
@@ -12,13 +12,37 @@ class Home extends StatelessWidget {
         title: const Text(
           'Flutter Flix!',
           style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-            fontFamily: 'Raleway-Regular',
-          ),
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Raleway-Bold',
+              fontSize: 32),
         ),
         centerTitle: true,
-        backgroundColor: Colors.red[400],
+        backgroundColor: Colors.grey[850],
+      ),
+      body: Container(
+        padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
+        child: const Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Flick Finder',
+                    style: TextStyle(
+                      fontSize: 24,
+                    ),
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Search Bar goes here!')
+                ],
+              )
+            ]),
       ),
     );
   }
